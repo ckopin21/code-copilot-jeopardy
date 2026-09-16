71d196655069f739ec5bb5813688f33ccfe65f4f
ckopin21 <ckopin21@gmail.com>
2026-09-16 13:46:26 -0500

    ui: load desktop menu layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './responsive-player-cards.css';
 import './endgame-menu-fixes.css';
+import './menu-desktop-layout.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
