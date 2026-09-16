5061ac19209599132ded81ae77e29cc7714d8ec6
ckopin21 <ckopin21@gmail.com>
2026-09-16 14:21:53 -0500

    refactor: remove fullscreen menu patch layer

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,5 @@ import './responsive-player-cards.css';
 import './endgame-menu-fixes.css';
 import './menu-desktop-layout.css';
 import './space-efficiency.css';
-import './fullscreen-menu-edge.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
