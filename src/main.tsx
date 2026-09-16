d1595c44b55ad7b23b426c43bb6d512f79fd8f7f
ckopin21 <ckopin21@gmail.com>
2026-09-16 13:51:53 -0500

    ui: load compact host layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import App from './App';
 import './responsive-player-cards.css';
 import './endgame-menu-fixes.css';
 import './menu-desktop-layout.css';
+import './space-efficiency.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
