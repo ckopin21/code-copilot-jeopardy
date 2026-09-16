79451b7b25b1e637c2623d2e1271726590deae21
ckopin21 <ckopin21@gmail.com>
2026-09-16 13:36:53 -0500

    ui: load endgame and menu fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './responsive-player-cards.css';
+import './endgame-menu-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
