37f6796fd8e88c43d2499c7785514eb4dc1d841f
ckopin21 <ckopin21@gmail.com>
2026-09-16 14:50:47 -0500

    fix: recover room storage before game engine boots

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './lib/roomStorageRecovery';
 import App from './App';
 import './responsive-player-cards.css';
 import './endgame-menu-fixes.css';
