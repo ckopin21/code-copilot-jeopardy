c2bca86e26736c6bb4ee7d7dbe8a5e83f682949d
ckopin21 <ckopin21@gmail.com>
2026-09-17 13:31:46 -0500

    feat: add presentation-mode preview to developer lab
    * feat: add presentation visual lab
    
    * style: add presentation lab viewport controls
    
    * feat: wire presentation preview into dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './lib/lobbySetupEnhancements';
 import './lib/hostFullscreenControl';
 import App from './App';
 import { DevModeOverlay } from './components/DevModeOverlay';
+import { DevPresentationLab } from './components/DevPresentationLab';
 import './responsive-player-cards.css';
 import './endgame-menu-fixes.css';
 import './menu-desktop-layout.css';
@@ -17,7 +18,8 @@ import './round-start-result-fixes.css';
 import './background-music.css';
 import './comeback-boosts.css';
 import './dev-mode.css';
+import './dev-presentation-lab.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/><DevModeOverlay/></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/><DevModeOverlay/><DevPresentationLab/></React.StrictMode>);
 
 // Late-loaded regression styles intentionally win cascade conflicts in game overlays.
