d6f7df4055573d9541a3c66593c1d12ec34ccd38
ckopin21 <ckopin21@gmail.com>
2026-09-18 09:02:29 -0500

    Add genre packs for Free Response
    * Add freeResponseGenreDisney Free Response pack
    
    * Add freeResponseGenreMoviesTv Free Response pack
    
    * Add freeResponseGenreMusic Free Response pack
    
    * Add freeResponseGenreHistoryGeography Free Response pack
    
    * Add freeResponseGenreSportsGames Free Response pack
    
    * Replace overlapping Free Response geography fact
    
    * Replace second overlapping Free Response history fact
    
    * Replace third overlapping Free Response history fact
    
    * Replace overlapping Alaska geography fact
    
    * Replace overlapping Mount Rushmore geography fact
    
    * Replace overlapping Maine geography fact
    
    * Replace overlapping Greenland geography fact
    
    * Replace overlapping Golden Gate geography fact
    
    * Replace overlapping Rio landmark fact
    
    * Replace overlapping Barcelona landmark fact
    
    * Replace overlapping TARDIS screen fact
    
    * Remove final duplicate fact from Free Response genre packs

---
+++ src/packs/freeResponseGenreDisney.ts
@@ -0,0 +1,63 @@
+import { buildPack, category, question } from './buildPack';
+
+const categories = [
+  category('Animated Adventures', {
+    100: question('In The Jungle Book, what is the name of Mowgli’s easygoing bear friend?', ['Baloo']),
+    200: question('In Tangled, what kingdom is Rapunzel the lost princess of?', ['Corona', 'Kingdom of Corona']),
+    300: question('In The Emperor’s New Groove, which emperor is transformed into a llama?', ['Kuzco', 'Emperor Kuzco']),
+    400: question('In Brother Bear, what is the name of the young bear who travels with Kenai?', ['Koda']),
+    500: question('In Treasure Planet, what is the first name of the teenage hero Jim Hawkins?', ['Jim']),
+    1000: question('Who is the villainous rat in The Great Mouse Detective?', ['Professor Ratigan', 'Ratigan'])
+  }),
+  category('Pixar Details', {
+    100: question('What is the name of the cowboy toy who leads Andy’s toys in Toy Story?', ['Woody', 'Sheriff Woody']),
+    200: question('What nickname do Sulley and Mike give the little girl in Monsters, Inc.?', ['Boo']),
+    300: question('What is the name of the red race car who stars in Cars?', ['Lightning McQueen', 'McQueen']),
+    400: question('In Up, what name does Russell give the large colorful bird?', ['Kevin']),
+    500: question('In Soul, what is the name of the jazz pianist whose soul gets separated from his body?', ['Joe Gardner', 'Joe']),
+    1000: question('In Luca, what is the full first name of Luca’s sea-monster best friend?', ['Alberto', 'Alberto Scorfano'])
+  }),
+  category('Heroes & Friends', {
+    100: question('What fairy is Peter Pan’s closest companion?', ['Tinker Bell', 'Tinkerbell']),
+    200: question('What is the name of the young teacup in Beauty and the Beast?', ['Chip']),
+    300: question('Who trains Hercules to become a hero?', ['Phil', 'Philoctetes']),
+    400: question('What is the name of Robin Hood’s large bear best friend?', ['Little John']),
+    500: question('In Bolt, what is the name of the streetwise cat who joins Bolt on his journey?', ['Mittens']),
+    1000: question('In Atlantis: The Lost Empire, what is the surname of the expedition commander who betrays the group?', ['Rourke', 'Commander Rourke', 'Lyle Rourke'])
+  }),
+  category('Villains', {
+    100: question('Who is the royal vizier villain in Aladdin?', ['Jafar']),
+    200: question('Who is Simba’s uncle and main enemy in The Lion King?', ['Scar']),
+    300: question('Who is Peter Pan’s pirate nemesis?', ['Captain Hook', 'Hook']),
+    400: question('What are the names of Cruella de Vil’s two henchmen?', ['Horace and Jasper', 'Jasper and Horace', 'Horace Badun and Jasper Badun']),
+    500: question('Who is the ruthless judge who pursues Esmeralda in The Hunchback of Notre Dame?', ['Claude Frollo', 'Judge Frollo', 'Frollo']),
+    1000: question('Who is the skeletal sorcerer villain in The Black Cauldron?', ['Horned King', 'The Horned King'])
+  }),
+  category('Parks & Attractions', {
+    100: question('Which Walt Disney World park is home to Cinderella Castle?', ['Magic Kingdom', 'The Magic Kingdom']),
+    200: question('What pirate-themed boat attraction inspired a film franchise starring Jack Sparrow?', ['Pirates of the Caribbean']),
+    300: question('What planet is the setting of Star Wars: Galaxy’s Edge?', ['Batuu']),
+    400: question('What Disneyland roller coaster is built around an artificial Alpine mountain?', ['Matterhorn Bobsleds', 'Matterhorn']),
+    500: question('How many national pavilions currently make up EPCOT’s World Showcase?', ['11', 'Eleven']),
+    1000: question('What is the name of the volcano landmark at Tokyo DisneySea?', ['Mount Prometheus', 'Prometheus'])
+  }),
+  category('Behind the Magic', {
+    100: question('What was Mickey Mouse originally going to be named before Walt Disney’s wife suggested Mickey?', ['Mortimer Mouse', 'Mortimer']),
+    200: question('Who voiced the Genie in Disney’s original animated Aladdin?', ['Robin Williams']),
+    300: question('What nickname is given to the group of veteran Disney animators known collectively as the “Nine Old Men”?', ['Nine Old Men', 'The Nine Old Men']),
+    400: question('Which songwriting brothers wrote Disney songs including “It’s a Small World” and music for Mary Poppins?', ['Sherman Brothers', 'The Sherman Brothers', 'Richard and Robert Sherman', 'Robert and Richard Sherman']),
+    500: question('Which 1961 Disney film made landmark use of Xerox technology to transfer animators’ drawings directly to cels?', ['One Hundred and One Dalmatians', '101 Dalmatians']),
+    1000: question('What multiplane-camera Disney film sequence is famous for the opening move through the forest in Bambi?', ['Bambi'])
+  })
+];
+
+export const freeResponseGenreDisneyPack = buildPack({
+  id: 'free-response-disney',
+  title: 'Free Response: Disney',
+  theme: 'Disney animation, Pixar, parks, villains, characters, and studio history',
+  description: 'A Disney-focused all-play pack with completely separate questions from the Classic Disney pack.',
+  difficulty: 'mixed',
+  approximateMinutes: 30,
+  supportedGameModes: ['free-response'],
+  accentColor: '#9ec5ff'
+}, categories);
---
+++ src/packs/freeResponseGenreMusic.ts
@@ -0,0 +1,63 @@
+import { buildPack, category, question } from './buildPack';
+
+const categories = [
+  category('Pop Hits', {
+    100: question('Who recorded the song “bad guy”?', ['Billie Eilish', 'Eilish']),
+    200: question('Who recorded “Firework”?', ['Katy Perry', 'Perry']),
+    300: question('Who recorded “Poker Face”?', ['Lady Gaga', 'Gaga']),
+    400: question('Who recorded “As It Was”?', ['Harry Styles', 'Styles']),
+    500: question('Who recorded “good 4 u”?', ['Olivia Rodrigo', 'Rodrigo']),
+    1000: question('Who recorded “Hips Don’t Lie”?', ['Shakira'])
+  }),
+  category('Rock & Alternative', {
+    100: question('Which band recorded “Smells Like Teen Spirit”?', ['Nirvana']),
+    200: question('Which band recorded “Boulevard of Broken Dreams”?', ['Green Day']),
+    300: question('Which band recorded “Wonderwall”?', ['Oasis']),
+    400: question('Which band recorded “Under the Bridge”?', ['Red Hot Chili Peppers', 'RHCP']),
+    500: question('Which band recorded “Everlong”?', ['Foo Fighters', 'The Foo Fighters']),
+    1000: question('Which band recorded “Take Me Out”?', ['Franz Ferdinand'])
+  }),
+  category('Hip-Hop', {
+    100: question('Which rapper recorded “SICKO MODE”?', ['Travis Scott', 'Scott']),
+    200: question('Which rapper recorded “God’s Plan”?', ['Drake']),
+    300: question('Which rapper recorded “California Love” with Dr. Dre?', ['Tupac Shakur', 'Tupac', '2Pac']),
+    400: question('Which rapper recorded “Gold Digger” with Jamie Foxx?', ['Kanye West', 'Kanye']),
+    500: question('Which hip-hop duo recorded “Ms. Jackson”?', ['Outkast']),
+    1000: question('Which rap group recorded “C.R.E.A.M.”?', ['Wu-Tang Clan', 'Wu Tang Clan'])
+  }),
+  category('Albums', {
+    100: question('Who released the album Thriller?', ['Michael Jackson']),
+    200: question('Which band released the album Rumours?', ['Fleetwood Mac']),
+    300: question('Which band released the album Nevermind?', ['Nirvana']),
+    400: question('Who released the album Back to Black?', ['Amy Winehouse', 'Winehouse']),
+    500: question('Who released The Miseducation of Lauryn Hill?', ['Lauryn Hill', 'Hill']),
+    1000: question('Who released the album Songs in the Key of Life?', ['Stevie Wonder', 'Wonder'])
+  }),
+  category('Bands & Groups', {
+    100: question('Which band recorded “Bohemian Rhapsody”?', ['Queen']),
+    200: question('Which band recorded “Hey Jude”?', ['The Beatles', 'Beatles']),
+    300: question('Which Irish band recorded “Zombie”?', ['The Cranberries', 'Cranberries']),
+    400: question('Which band recorded “Somebody Told Me”?', ['The Killers', 'Killers']),
+    500: question('Which band recorded “Basket Case”?', ['Green Day']),
+    1000: question('Which band recorded “No One Knows”?', ['Queens of the Stone Age', 'QOTSA'])
+  }),
+  category('Music Terms', {
+    100: question('What musical dynamic marking means loud?', ['Forte']),
+    200: question('What musical term means to gradually get louder?', ['Crescendo']),
+    300: question('What is a chord made of three notes commonly called?', ['Triad', 'A triad']),
+    400: question('What interval spans from one note to the next note with the same letter name?', ['Octave', 'An octave']),
+    500: question('Which major key has no sharps or flats in its key signature?', ['C major', 'C']),
+    1000: question('What traditional Italian tempo marking means very slow and broad?', ['Largo'])
+  })
+];
+
+export const freeResponseGenreMusicPack = buildPack({
+  id: 'free-response-music',
+  title: 'Free Response: Music',
+  theme: 'Pop, rock, hip-hop, albums, bands, and music terminology',
+  description: 'A music all-play pack with new songs, artists, albums, and theory facts separate from Classic.',
+  difficulty: 'mixed',
+  approximateMinutes: 30,
+  supportedGameModes: ['free-response'],
+  accentColor: '#f2a7ff'
+}, categories);
