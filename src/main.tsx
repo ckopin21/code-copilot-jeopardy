6bfa60077c4d0e40cf517b230f53a5f4dbc1e1a6
ckopin21 <ckopin21@gmail.com>
2026-09-19 14:07:53 -0500

    refactor: remove redundant PRES LAB entry point

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import App from './App';
 import { applyMusicVolumePolicy } from './lib/musicVolumePolicy';
 import { installModeAwareAudioPolicy } from './lib/audioModePolicy';
 import { DevModeOverlay } from './components/DevModeOverlay';
-import { DevPresentationLab } from './components/DevPresentationLab';
 import './responsive-player-cards.css';
 import './endgame-menu-fixes.css';
 import './menu-desktop-layout.css';
@@ -34,6 +33,6 @@ import './ui-layout-audit-fixes.css';
 applyMusicVolumePolicy();
 installModeAwareAudioPolicy();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/><DevModeOverlay/><DevPresentationLab/></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/><DevModeOverlay/></React.StrictMode>);
 
 // Late-loaded regression styles intentionally win cascade conflicts in game overlays.
